872986f9bb2dff437e5ee26ea0f7691678468baf
William <williamhumbwavali@gmail.com>
2024-10-10 12:55:28 +0100

    Refactor index file: Remove direct imports for controllers and models to be loaded by autoload, retain middleware import, and streamline application initialization

--- public/index.php
+++ public/index.php
@@ -14,10 +14,8 @@ require __DIR__ . '/../vendor/autoload.php';
 // Load the database configuration
 define('DB_CONNECTION', require PROJECT_ROOT . '/database/config/database.php');
 
-// Import all models, middleware, and controllers
-import::dir(PROJECT_ROOT . '/models')->get();
+// Import all middleware
 import::dir(PROJECT_ROOT . '/Http/Middleware')->get();
-import::dir(PROJECT_ROOT . '/Http/Controllers')->get();
 
 // Start the application
 require PROJECT_ROOT . '/App.php';
